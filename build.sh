f36157c86d41951103359c0e7f7df5303fd4eefd
exurd <7785022+exurd@users.noreply.github.com>
2025-03-09 22:46:46 +0000

    create linux build script
    closes #11

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+echo "Building program..."
+source /etc/profile.d/devkit-env.sh
+make clean && make
+
+
+echo
+echo "Creating release..."
+if [ -d "release" ]; then
+    rm -rf "release"
+fi
+mkdir "release"
+
+
+echo
+echo "'apps' folder..."
+mkdir -p "./release/apps/WiiLikeToParty"
+cp ./WiiLikeToParty.dol ./release/apps/WiiLikeToParty/boot.dol
+cp ./WiiLikeToParty.elf ./release/apps/WiiLikeToParty/alt_for_nerds.elf
+cp ./assets/hb/* ./release/apps/WiiLikeToParty/
+
+
+echo
+echo "'wad' folder..."
+mkdir -p "./release/wad"
+cp ./wad/ios/*.wad ./release/wad/
+cp ./wad/forwarder_WL2P.wad ./release/wad/
+
+
+echo
+echo "Compressing release..."
+GIT_VERSION=$(git describe --tags --always)
+ZIP_FILE="WiiLikeToParty_$GIT_VERSION.zip"
+if [ -f "./$ZIP_FILE" ]; then
+    rm -f "./$ZIP_FILE"
+fi
+# sudo apt install zip
+cd ./release; zip -r9 "../$ZIP_FILE" *
+
+echo
+echo Done!
